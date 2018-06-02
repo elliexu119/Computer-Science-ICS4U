40c663b574ddaddc6bb1f89a02de1f489801a7af
ellix <ellix@199.235.250.161>
2018-06-01 21:59:46 -0400

    need to fix boolean

--- ICS4U-Ellie/src/unit7_records/Main.java
+++ ICS4U-Ellie/src/unit7_records/Main.java
@@ -9,15 +9,36 @@ package unit7_records;
  *
  * @author 1xuell
  */
+import java.io.*;
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Records a = new Records ("s", "asdf", 2001);
-        a.together(); 
+    public static void main(String[] args) throws Exception {
+        Records a = new Records("Eye of the tiger", "Survivor", 1982, 3.5, true);
+        Records b = new Records("Thunderstruck", "AC/DC", 1990, 3.2, false);
+        Records c = new Records("Back in black", "AC/DC", 1980, 5, false);
+        Records master = new Records(); 
+
+        master.write(a); 
+        master.write(b);
+        master.write(c);
+        master.read(0, a); 
+        System.out.println("");
+        master.read(master.length()/3, b);
+        System.out.println("");
+        master.read(master.length()/3*2  , c);
+
+        
+        //read(0, a, file);
+        //read(a.RECORD_SIZE, b, file);
+//        for (int i = 59; i < b.SONG_SIZE * 2 + 59 ; i = i + 2) {
+//            file.seek(i);
+//            System.out.print(file.readChar());
+//        }
+        
         
     }
-    
 }
--- ICS4U-Ellie/src/unit7_records/Records.java
+++ ICS4U-Ellie/src/unit7_records/Records.java
@@ -5,24 +5,97 @@
  */
 package unit7_records;
 
+import java.io.RandomAccessFile;
+
 /**
  *
  * @author 1xuell
  */
 public class Records {
 
-    protected final int RECORD_SIZE = 58;
-    protected final int SONG_SIZE = 39;
+    protected final int SONG_SIZE = 36;
     protected final int WRITER_SIZE = 15;
-    protected final int YEAR_SIZE = 4; 
+    protected final int YEAR_SIZE = 4;
+    protected final int DURATION_SIZE = 3;
+    protected final int FAVOURITE_SIZE = 10;
+    protected final int RECORD_SIZE = 115; //(SONG_SIZE + WRITER_SIZE)*2 + YEAR_SIZE + RATING_SIZE + 1;
     private String song;
     private String writer;
-    private String year; 
+    private int year;
+    private double duration;
+    private boolean favourite;
+    private RandomAccessFile file;
+    //private char
 
-    public Records(String song, String writer, String year) {
+    public Records(String song, String writer, int year, double duration, boolean favourite) {
         this.song = song;
         this.writer = writer;
         this.year = year;
+        this.duration = duration;
+        this.favourite = favourite;
+    }
+
+    public Records() throws Exception {
+        file = new RandomAccessFile("song_info.txt", "rw");
+    }
+    
+    public int length()throws Exception {
+        return (int) file.length(); 
+    }
+
+    public void write(Records records) throws Exception {
+        file.writeChars(records.getSong());
+        file.writeChars(records.getWriter());
+        file.writeInt(records.getYear());
+        file.writeDouble(records.getDuration());
+        file.writeBoolean(records.getFavourite());
+    }
+
+    public void read(int begin, Records records) throws Exception {
+        int i = begin;
+        for (; i < records.SONG_SIZE * 2 + begin; i = i + 2) {
+            file.seek(i);
+            System.out.print(file.readChar());
+        }
+        System.out.println("");
+        for (; i < (records.SONG_SIZE + records.WRITER_SIZE) * 2 + begin; i = i + 2) {
+            file.seek(i);
+            System.out.print(file.readChar());
+        }
+        System.out.println("");
+
+        file.seek((records.SONG_SIZE + records.WRITER_SIZE) * 2 + begin);
+        System.out.println(file.readInt());
+
+        file.seek((records.SONG_SIZE + records.WRITER_SIZE) * 2 + records.YEAR_SIZE + begin);
+        System.out.println(file.readDouble());
+
+        file.seek((records.SONG_SIZE + records.WRITER_SIZE) * 2 + records.YEAR_SIZE + records.DURATION_SIZE + 5 + begin);
+        System.out.println(file.readBoolean());
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public void setFavourite(boolean favourite) {
+        this.favourite = favourite;
+    }
+
+    public double getDuration() {
+        return Double.parseDouble(methodNum(duration, DURATION_SIZE));
     }
 
     public String getSong() {
@@ -35,24 +108,36 @@ public class Records {
         return writer;
     }
 
-    public String getYear() {
-        method (year, YEAR_SIZE);
-        return year;
+    public int getYear() {
+        return Integer.parseInt(methodNum(year, YEAR_SIZE));
     }
-    
-    public void together(){
-        
-        System.out.println(song.length());
-    }
-    
-    public String method (String a, int b ){
+
+    public boolean getFavourite() {
         StringBuilder temp = new StringBuilder();
-        if ( a != null ) {
-            temp.append( a.trim() );
+        temp.append(favourite);
+        temp.setLength(FAVOURITE_SIZE);
+        return Boolean.parseBoolean(temp.toString());
+    }
+
+    public String method(String a, int b) {
+        StringBuilder temp = new StringBuilder();
+        if (a != null) {
+            temp.append(a.trim());
         } else {
-            temp.append( "TBD" );
+            temp.append("TBD");
         }
-        temp.setLength( b );
-        return temp.toString(); 
+        temp.setLength(b);
+        return temp.toString();
+    }
+
+    public String methodNum(double a, int b) {
+        StringBuilder temp = new StringBuilder();
+        if (a != 0) {
+            temp.append(a);
+        } else {
+            temp.append("TBD");
+        }
+        temp.setLength(b);
+        return temp.toString();
     }
 }
